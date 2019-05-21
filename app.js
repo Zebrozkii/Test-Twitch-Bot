698ec8fdeee49a932e864b3e78550381e311d8c8
Jimmy Zebroski <zebrozkii@gmail.com>
2019-05-21 11:26:19 -0700

    added first mod function

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ var options = {
 var client = new tmi.client(options);
 client.connect();
 client.on('chat', function(channel, user, message, self){
-  client.action("zebrozkii ", user ['display-name'] + " You are a Trash Can");
+  // client.action("zebrozkii ", user ['display-name'] + " You are a Trash Can");
 });
 
 client.on('connected', function (address , port){
  client.action("zebrozkii", "Hello Im Testing my bot");
 });
+
+client.on("chat", (channel, user, message, self)=>{
+  if(user.mod || user['usertype'] === 'mod'){
+    client.action("zebrozkii", `You suck ${user.username}`);
+  }
+});
+
